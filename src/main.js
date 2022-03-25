f21afd98aa276d2e7017121699e1c696378da9d5
zengqiangHe <zengqiang.he@outlook.com>
2022-03-25 15:54:42 +0100

    add load-data js

---
+++ src/main.js
@@ -0,0 +1,33 @@
+
+const button = document.querySelector('#fetch-data');
+console.clear();
+const baseUrl = 'https://rickandmortyapi.com/api/character?page=';
+const numPages = 5;
+
+const urls = Array(numPages)
+        .fill() // [undefined, undefined, ...]
+        .map((_, index) => baseUrl + (index + 1));
+
+button.addEventListener('click',() => {
+const promises = urls.map(url => fetch(url).then(res => res.json()))
+console.log(promises);
+
+Promise
+.all(promises)
+.then(pages => {
+const characters = pages.flatMap(page => page.results)
+characters.forEach(renderCharacter)
+})
+});
+
+
+function renderCharacter(character) {
+const frame = document.createElement('div');
+const img = document.createElement('img');
+img.setAttribute('src', character.image);
+frame.append(img);
+const name = document.createElement('p');
+name.textContent = character.name;
+frame.append(name);
+document.querySelector('#container').append(frame)
+}
