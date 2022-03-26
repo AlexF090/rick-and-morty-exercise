5045b8f82f3f82e62a8225af1d4514c05506b7a0
Alexander Fischenich <alexf@airvonalexander.speedport.ip>
2022-03-26 13:03:44 +0100

    add all specs to char cards

--- src/main.js
+++ src/main.js
@@ -20,12 +20,50 @@ button.addEventListener("click", () => {
 function renderCharacter(character) {
   const frame = document.createElement("li");
   const img = document.createElement("img");
-  const name = document.createElement("p");
+  const name = document.createElement("h2");
+  const id = document.createElement("h3");
+  const status = document.createElement("p");
+  const species = document.createElement("p");
+  const type = document.createElement("p");
+  const gender = document.createElement("p");
+  const originLocation = document.createElement("p");
+  const endpointLocation = document.createElement("p");
+  const listOfEpisode = document.createElement("p");
+  const createdInDatabase = document.createElement("p");
   frame.classList.add("card");
+  //Profile picture
   img.setAttribute("src", character.image);
   img.setAttribute("alt", "Profile picture");
+  //Char Name
   name.textContent = character.name;
+  //Char ID
+  id.textContent = character.id;
+  //Char status
+  status.textContent = character.status;
+  //Char species
+  species.textContent = character.species;
+  //Char type
+  type.textContent = character.type;
+  //Char gender
+  gender.textContent = character.gender;
+  // originLocation
+  originLocation.textContent = character.origin.name;
+  //endpointLocation
+  endpointLocation.textContent = character.location.name;
+  // listOfEpisode
+//Later!!!
+  //createdInDatabase
+  createdInDatabase.textContent = character.created;
   frame.append(img);
+  frame.append(id);
   frame.append(name);
+  frame.append(status);
+  frame.append(species);
+  frame.append(type);
+  frame.append(gender);
+  frame.append(originLocation);
+  frame.append(endpointLocation);
+  frame.append(listOfEpisode);
+  frame.append(createdInDatabase);
   document.querySelector("#container").append(frame);
 }
