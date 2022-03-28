6ca28e51ec2a425dfa439e522423586145011f41
zengqiangHe <zengqiang.he@outlook.com>
2022-03-28 13:05:37 +0200

    add mediaQuery

--- src/main.js
+++ src/main.js
@@ -4,11 +4,6 @@ const dropdown = document.querySelector("#header__filter");
 const baseUrl = "https://rickandmortyapi.com/api/character?page=";
 const numPages = 5;
 
-
-// console.log(dropdown)
-
-// function filterStatus ()
-
 // Fetch data
 
 const urls = Array(numPages)
@@ -17,6 +12,7 @@ const urls = Array(numPages)
 
 button.addEventListener("click", () => {
   document.querySelector('#container').innerHTML='';
+
   const promises = urls.map((url) => fetch(url).then((res) => res.json()));
   console.log(promises);
 
@@ -28,54 +24,32 @@ button.addEventListener("click", () => {
 });
 
 // Render Cards
-// character.status &&
 function renderCharacter(character) {
-  // if (dropdown.value === character.status && character.name.includes(document.querySelector('#name').value)){
   const frame = document.createElement("li");
   const img = document.createElement("img");
   const name = document.createElement("h2");
-  const id = document.createElement("h3");
+  
   const status = document.createElement("p");
-  const species = document.createElement("p");
-  const type = document.createElement("p");
+ 
   const gender = document.createElement("p");
-  const originLocation = document.createElement("p");
-  const endpointLocation = document.createElement("p");
-  const listOfEpisode = document.createElement("p");
-  const createdInDatabase = document.createElement("p");
+ 
+
+  
+ 
   frame.classList.add("card");
   //Profile picture
   img.setAttribute("src", character.image);
   img.setAttribute("alt", "Profile picture");
   //Char Name
   name.textContent = character.name;
-  //Char ID
-  id.textContent = `ID: ${character.id}`;
   //Char status
   status.textContent = `Status: ${character.status}`;
-  //Char species
-  species.textContent = `Species: ${character.species}`;
-  //Char type
-  type.textContent = `Type: ${character.type}`;
   //Char gender
   gender.textContent = `Gender: ${character.gender}`;
-  // originLocation
-  originLocation.textContent = `Origin location: ${character.origin.name}`;
-  //endpointLocation
-  endpointLocation.textContent = `End point location: ${character.location.name}`;
-  //createdInDatabase
-  createdInDatabase.textContent = `Created in Database: ${character.created}`;
   frame.append(img);
-  frame.append(id);
   frame.append(name);
   frame.append(status);
-  frame.append(species);
-  frame.append(type);
   frame.append(gender);
-  frame.append(originLocation);
-  frame.append(endpointLocation);
-  frame.append(listOfEpisode);
-  frame.append(createdInDatabase);
   document.querySelector("#container").append(frame);
 }
 // }
@@ -83,40 +57,16 @@ function renderCharacter(character) {
 // Filter Cards
 
 function filterCharacter(character) {
-
-// const isNameFilterMatching = character.name.includes(document.querySelector('#name').value) || document.querySelector('#name') === '';
-// const isStatusFilterMatching = character.status
-
-// if(isNameFilterMatching && isStatusFilterMatching){
-//   renderCharacter(character)
-// }
-// if(dropdown.value !== 'All' && document.querySelector('#name').value !== '') {
-// renderCharacter(character)
-// }
-    if (dropdown.value === 'All' && character.name.includes(document.querySelector('#name').value)){
+  const characterSearch = character.name.includes(document.querySelector('#name').value);
+    if (dropdown.value === 'All' && characterSearch){
       renderCharacter(character)
     }
-if (dropdown.value === character.status && character.name.includes(document.querySelector('#name').value)){
+else if(dropdown.value === character.status && characterSearch){
         renderCharacter(character)
       }
-    // else if (dropdown.value === character.status || character.name.includes(document.querySelector('#name').value)){
-    //   renderCharacter(character)
-    //   console.log('1. else if')
-    // }
-//     else if (dropdown.value === 'All'){
-//       renderCharacter(character)
-// }
 }
 
-
-
 //Top Button
-
-// const container = document.querySelector("#container");
-// window.addEventListener('scroll', () => {
-//   const topButton = document.querySelector('[data-btn=topButton]');
-//   topButton.window.
-// });
 
 window.addEventListener("scroll", () => {
   var scroll = document.querySelector(".scrollTop");
